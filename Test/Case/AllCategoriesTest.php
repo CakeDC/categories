2ca4f00032954d22ccbce46f389bc2f6d9b3f4e6
Florian Krämer <florian.kraemer@cakedc.com>
2014-05-11 22:46:48 +0200

    Adding AllCategoriesTest.php

---
+++ Test/Case/AllCategoriesTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright 2009 - 2014, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2009 - 2014, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+class AllMigrationsPluginTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$Suite = new CakeTestSuite('All Categories tests');
+		$path = dirname(__FILE__);
+		$Suite->addTestDirectory($path . DS . 'Controller');
+		$Suite->addTestDirectory($path . DS . 'Model');
+		return $Suite;
+	}
+
+}
